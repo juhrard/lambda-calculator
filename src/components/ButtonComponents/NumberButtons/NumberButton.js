4d47ee3330cabf2ab97be85a28700a7ffe325331
Gerard Crockenberg <gmahred@gmail.com>
2019-12-10 16:11:28 -0500

    calculator finished + stretch

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
+const NumberButton = (props) => {
+  return ( 
+    <button style={getNButtonStyles(props.number)} className="numberButton button">
+      {props.number}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+let getNButtonStyles = function(num) {
+  return {
+    boxSizing: 'border-box',
+    borderRadius: '50px',
+    backgroundColor: 'darkblue',
+    width: num === '0' ? '200px' : '90px',
+    height: '90px',
+    color: 'white',
+    margin: '10px',
+    marginTop: '8px',
+    marginBottom: '8px',
+    display: 'flex',
+    fontFamily: 'Arial',
+    fontSize: '2.5rem',
+    justifyContent: 'center',
+  }
+}
+
+
+
+export default NumberButton;
+
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
-    <>
+    <button style={oButtonStyles} className="operatorButton button">
+      {props.operator}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+const oButtonStyles = {
+  boxSizing: 'border-box',
+  borderRadius: '50px',
+  backgroundColor: 'dodgerblue',
+  width: '90px',
+  height: '90px',
+  color: 'white',
+  margin: '10px',
+  marginTop: '8px',
+  marginBottom: '8px',
+  display: 'flex',
+  justifyContent: 'center',
+  textAlign: 'center',
+  fontFamily: 'Arial',
+  fontSize: '2.5rem',
+}
+
+export default OperatorButton;
